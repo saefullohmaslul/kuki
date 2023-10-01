7c0bd7df66041fedef65fffae849464974963ce8
saufiroja <saufiroja08@gmail.com>
2023-10-01 16:31:40 +0700

    feat: add rest api logic

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ gen-protoc:
 	@go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 	@go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 	@echo "Generating protobuf files..."
-	@protoc -I ./internal/apps/grpc --go_out=./internal/apps ./internal/apps/grpc/proto/*.proto
-	@protoc -I ./internal/apps/grpc --go-grpc_out=./internal/apps ./internal/apps/grpc/proto/*.proto
+	@protoc -I ./internal/apps/grpc --go_out=./internal/apps ./proto/*.proto
+	@protoc -I ./internal/apps/grpc --go-grpc_out=./internal/apps ./proto/*.proto
 
 	@protoc -I ./ --go_out=./ ./proto/*.proto
 	@protoc -I ./ --go-grpc_out=./ ./proto/*.proto
@@ -18,3 +18,13 @@ build:
 .PHONY: run
 run:
 	@./dist/kuki
+
+protoc:
+	protoc -I . \
+	--grpc-gateway_out . \
+	--grpc-gateway_opt logtostderr=true \
+	--grpc-gateway_opt generate_unbound_methods=true \
+	--go-grpc_out=. \
+	--go_out=. \
+	./proto/*.proto \
+	./internal/modules/google/api/*.proto
--- internal/modules/main.go
+++ internal/modules/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"os"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/color"
 	"github.com/saefullohmaslul/kuki/internal/modules/todos"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -24,17 +29,47 @@ func main() {
 		panic(err)
 	}
 
-	todosGrpcHandler := todos.NewTodosGrpcHandler()
-
 	opt := []grpc.ServerOption{}
-
 	server := grpc.NewServer(opt...)
+	reflection.Register(server)
+
+	todosGrpcHandler := todos.NewTodosGrpcHandler()
 
 	todos.RegisterTodosHandlerServer(server, todosGrpcHandler)
 
-	fmt.Printf("⇨ grpc server started on %s\n", color.New().Magenta(listen.Addr()))
+	fmt.Printf("⇨ grpc server started on %s\n", color.New().Magenta(fmt.Sprintf(":%s", port)))
 
-	err = server.Serve(listen)
+	go func() {
+		err := server.Serve(listen)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	conn, err := grpc.DialContext(
+		context.Background(),
+		"0.0.0.0:3001",
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	app := echo.New()
+	gwmux := runtime.NewServeMux()
+
+	err = todos.RegisterTodosHandlerHandler(context.Background(), gwmux, conn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	app.Any("*", echo.WrapHandler(gwmux))
+
+	fmt.Printf("⇨ http server started on %s\n", color.New().Magenta(":8080"))
+
+	err = app.Start(":8080")
 	if err != nil {
 		panic(err)
 	}
