193ffda3897ab5a366155903766de885bd1f5f9a
saufiroja <saufiroja08@gmail.com>
2023-10-11 23:25:49 +0700

    feat: config database connection

--- go.mod
+++ go.mod
@@ -10,10 +10,17 @@ require (
 	go.uber.org/fx v1.20.0
 	google.golang.org/grpc v1.58.0
 	google.golang.org/protobuf v1.31.0
+	gorm.io/driver/postgres v1.5.3
+	gorm.io/gorm v1.25.2-0.20230530020048-26663ab9bf55
 )
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
---
+++ internal/pkg/database/postgres.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Database struct {
+	*gorm.DB
+}
+
+func NewDatabase() Database {
+	var (
+		connection *gorm.DB
+		err        error
+	)
+
+	_ = godotenv.Load(".env")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Shanghai",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSL"),
+	)
+
+	connection, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return Database{
+		DB: connection,
+	}
+}
