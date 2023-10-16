ae1d1d2cb9499326a56db88915de4fdfe4e62676
saufiroja <saufiroja08@gmail.com>
2023-10-17 01:18:01 +0700

    fix: fix struct

--- internal/models/todos_model.go
+++ internal/models/todos_model.go
@@ -2,7 +2,7 @@ package models
 
 // Todos is struct to represent todos table
 type Todos struct {
-	TodoID      string `json:"todo_id;primaryKey"`
+	TodoID      string `json:"todo_id" gorm:"primaryKey"`
 	Title       string `json:"title" validate:"required"`
 	Description string `json:"description" validate:"omitempty"`
 	Completed   bool   `json:"completed" validate:"omitempty"`
